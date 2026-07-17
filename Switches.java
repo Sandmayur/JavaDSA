8d9192dddbe2fdc5326d75b19602007ae02fb5a3
Sandmayur <mayurdhumal29@gmail.com>
2026-07-17 14:25:29 +0530

    Initial commit

---
+++ Switches.java
@@ -0,0 +1,34 @@
+public class Switches {
+    public static void main (String []args)
+    {
+        int n = 8;
+        switch(n)
+        {case 1:
+            System.out.println("Sunday ");
+            break;
+            case 2 :
+                System.out.println("Monday");
+                break;
+                 case 3:
+                System.out.println("Tuesday");
+                break;
+                case 4:
+                    System.out.println("Wed");
+                    break;
+                    case 5:
+                    System.out.println("Thu");
+                    break;
+                    case  6:
+                    System.out.println("Fri");
+                    break;
+                    case 7:
+                    System.out.println("Sat");
+                    break;
+
+                    default:
+                    System.out.println("Please enter valid number ");
+        }
+        
+    
+}
+}
